538c7821933312a583424d75eb769e9c91adeb0a
jvitela <j_vitel@yahoo.com.mx>
2015-10-06 12:31:34 +0200

    Added String.prototype.trim polyfill to tests.js

--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,9 @@
+if (!String.prototype.trim) {
+  String.prototype.trim = function () {
+    return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
+  };
+}
+
 Mustache.Formatters = {
 	"uppercase": function (str) {
 		return str.toUpperCase();
