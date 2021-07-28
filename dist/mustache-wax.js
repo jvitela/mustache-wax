c67e3f3c0acb0b485aa1218c47b72559c291fb7f
jvitela <j.vitel@gmail.com>
2021-07-28 10:00:47 +0200

    rebuilt

--- dist/mustache-wax.js
+++ dist/mustache-wax.js
@@ -8,104 +8,131 @@
 	else
 		root["wax"] = factory();
 })(this, function() {
-return /******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// define __esModule on exports
-/******/ 	__webpack_require__.r = function(exports) {
-/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 		}
-/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 	};
-/******/
-/******/ 	// create a fake namespace object
-/******/ 	// mode & 1: value is a module id, require it
-/******/ 	// mode & 2: merge all properties of value into the ns
-/******/ 	// mode & 4: return value when already ns object
-/******/ 	// mode & 8|1: behave like require
-/******/ 	__webpack_require__.t = function(value, mode) {
-/******/ 		if(mode & 1) value = __webpack_require__(value);
-/******/ 		if(mode & 8) return value;
-/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
-/******/ 		var ns = Object.create(null);
-/******/ 		__webpack_require__.r(ns);
-/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
-/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
-/******/ 		return ns;
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
-/******/
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
-/******/ })
-/************************************************************************/
-/******/ ({
+return /******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/*! no static exports found */
-/***/ (function(module, exports) {
+/***/ ((module) => {
 
-eval("function wax(Mustache, Formatters = {}) {\n    Mustache.Formatters = Formatters;\n\n    /*\n     *\tThis will parse a parameter from a filter:\n     *  \n     *      {{ vaue | filter : param1 : param2 : param3 }}\n     */\n    function parseParam(param, lookup) {\n        var isString, isInteger, isFloat;\n        isString  = /^[\\'\\\"](.*)[\\'\\\"]$/g;\n        isInteger = /^[+-]?\\d+$/g;\n        isFloat   = /^[+-]?\\d*\\.\\d+$/g;\n        if (isString.test(param)) {\n            return param.replace(isString, '$1');\n        }\n        if (isInteger.test(param)) {\n            return parseInt(param, 10);\n        }\n        if (isFloat.test(param)) {\n            return parseFloat(param);\n        }\n        return lookup(param);\n    };\n\n    /*\n     *\tThis function will resolve one filter# in the mustache expression:\n     *  \n     *      {{ value | filter1 | filter2 | ... | filterN }}\n     */\n    function applyFilter(expr, fltr, lookup) {\n        var filterExp, paramsExp, match, filter, params = [expr];\n        filterExp = /^\\s*([^\\:]+)/g;\n        paramsExp = /\\:\\s*([\\'][^\\']*[\\']|[\\\"][^\\\"]*[\\\"]|[^\\:]+)\\s*/g;\n        match = filterExp.exec( fltr);\n        filter = match[1].trim();\n        while ((match = paramsExp.exec(fltr))) {\n            params.push(parseParam(match[1].trim(), lookup));\n        }\n\n        if (Mustache.Formatters.hasOwnProperty(filter)) {\n            fltr = Mustache.Formatters[filter];\n            return fltr.apply(fltr, params);\n        }\n        return expr;\n    };\n\n    /*\n    * Keep a copy of the original lookup function of Mustache\n    */\n    const lookup = Mustache.Context.prototype.lookup;\n\n    /*\n    * Overwrite the Context::lookup method to add filter capabilities\n    */\n    Mustache.Context.prototype.lookup = function parseExpression(name) {\n        const formatters = name.split(\"|\");\n        let expression = formatters.shift().trim();\n        // call original lookup method\n        expression = lookup.call(this, expression);\n        // Apply the formatters\n        for (let i = 0, l = formatters.length; i < l; ++i) {\n            expression = applyFilter(expression, formatters[i], this.lookup.bind(this));\n        }\n        return expression;\n    };\n\n    return Mustache;\n}\n\n// Check if mustache was included globally\ntry {\n    if (Mustache) {\n        Mustache = wax(Mustache);\n    }\n} catch (err) { }\n\nmodule.exports = wax;\n\n//# sourceURL=webpack://wax/./src/index.js?");
+function wax(Mustache, Formatters = {}) {
+    Mustache.Formatters = Formatters;
+
+    /*
+     *	This will parse a parameter from a filter:
+     *  
+     *      {{ vaue | filter : param1 : param2 : param3 }}
+     */
+    function parseParam(param, lookup) {
+        var isString, isInteger, isFloat;
+        isString  = /^[\'\"](.*)[\'\"]$/g;
+        isInteger = /^[+-]?\d+$/g;
+        isFloat   = /^[+-]?\d*\.\d+$/g;
+        if (isString.test(param)) {
+            return param.replace(isString, '$1');
+        }
+        if (isInteger.test(param)) {
+            return parseInt(param, 10);
+        }
+        if (isFloat.test(param)) {
+            return parseFloat(param);
+        }
+        return lookup(param);
+    };
+
+    /*
+     *	This function will resolve one filter# in the mustache expression:
+     *  
+     *      {{ value | filter1 | filter2 | ... | filterN }}
+     */
+    function applyFilter(expr, fltr, lookup) {
+        var filterExp, paramsExp, match, filter, params = [expr];
+        filterExp = /^\s*([^\:]+)/g;
+        paramsExp = /\:\s*([\'][^\']*[\']|[\"][^\"]*[\"]|[^\:]+)\s*/g;
+        match = filterExp.exec( fltr);
+        filter = match[1].trim();
+        while ((match = paramsExp.exec(fltr))) {
+            params.push(parseParam(match[1].trim(), lookup));
+        }
+
+        if (Mustache.Formatters.hasOwnProperty(filter)) {
+            fltr = Mustache.Formatters[filter];
+            return fltr.apply(fltr, params);
+        }
+        return expr;
+    };
+
+    /*
+    * Keep a copy of the original lookup function of Mustache
+    */
+    const lookup = Mustache.Context.prototype.lookup;
+
+    /*
+    * Overwrite the Context::lookup method to add filter capabilities
+    */
+    Mustache.Context.prototype.lookup = function parseExpression(name) {
+        const formatters = name.split("|");
+        let expression = formatters.shift().trim();
+        // call original lookup method
+        expression = lookup.call(this, expression);
+        // Apply the formatters
+        for (let i = 0, l = formatters.length; i < l; ++i) {
+            expression = applyFilter(expression, formatters[i], this.lookup.bind(this));
+        }
+        return expression;
+    };
+
+    return Mustache;
+}
+
+// Check if mustache was included globally
+try {
+    if (Mustache) {
+        Mustache = wax(Mustache);
+    }
+} catch (err) { }
+
+module.exports = wax;
 
 /***/ })
 
-/******/ });
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
 });
+//# sourceMappingURL=mustache-wax.js.map
